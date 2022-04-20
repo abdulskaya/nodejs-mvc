8b78e0ba4e318a28364f32b96e5425e90a059818
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-20 05:24:07 +0300

    add src & public folders

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const connection = require('./config/db');
+const connection = require('./src/config/db');
 const env = require('dotenv').config();
 
 // Set template engine
 app.set('view engine', 'ejs');
 
+app.use(express.static('public')); 
+
 // Routes
-app.use('/', require('./routes/routes'));
+app.use('/', require('./src/routes/routes'));
 
 const PORT = process.env.SERVER_PORT || 4111;
 app.listen(PORT, console.log("Server has started at port " + PORT))
