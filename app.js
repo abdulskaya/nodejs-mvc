b6395e12e10ac9381a764fe806bf8904d0c6d931
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-20 05:18:56 +0300

    connect db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const connection = require('./config/db');
+const env = require('dotenv').config();
 
 // Set template engine
 app.set('view engine', 'ejs');
@@ -7,5 +9,5 @@ app.set('view engine', 'ejs');
 // Routes
 app.use('/', require('./routes/routes'));
 
-const PORT = process.env.PORT || 4111;
+const PORT = process.env.SERVER_PORT || 4111;
 app.listen(PORT, console.log("Server has started at port " + PORT))
